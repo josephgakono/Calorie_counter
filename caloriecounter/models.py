4fcd96fbf0dc177f02fa15e9dfadf9d6ad4ab8ad
josephgakono <sirg4kono@gmail.com>
2026-05-25 13:29:45 +0300

    Add food item model

--- caloriecounter/models.py
+++ caloriecounter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class FoodItem(models.Model):
+    """A food eaten today and its calorie amount."""
+
+    name = models.CharField(max_length=100)
+    calories = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.calories} calories"
